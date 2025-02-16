6cc8621aeb1d0f602dd4391ee9d521a343b9bafd
abc <abc@gmail.com>
2025-02-16 18:49:28 +0000

    modified filename and salary script

--- checkfilename.sh
+++ checkfilename.sh
@@ -2,7 +2,12 @@ echo "Enter a filename to check whether it is present or not "
 read fn
 if [ -f $fn ];
 then 
-echo "File exists"
+    if [ ! -s $fn ];
+    then
+        echo "File exists but data is not present"
+    else
+        echo "File exists and data is present"
+    fi
 else 
 echo "File does not exists"
 fi
--- checksalary.sh
+++ checksalary.sh
@@ -1,8 +1,11 @@
 echo "Enter your salary to know the tax ammount"
 read sal
-if [ $sal -le 30000 ];
+if [ $sal -lt 30000 ];
 then 
 echo " NO tax "
+elif [ $sal -eq 30000 ]
+then 
+echo " 5% tax = rs $(( $sal*5/100 )) "
 elif [ $sal -gt 30000 -a $sal -le 40000 ]
 then 
 echo " 10% tax = rs $(( $sal*10/100 )) "
